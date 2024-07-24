0b8c02a5ca094111bef53ad2f263132c521bb429
vish-codes <anand.panorama@gmail.com>
2024-07-24 16:09:33 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -38,7 +38,7 @@ router.put("/reAssign/:id", async (req, res) => {
   const getLaptopUser = await Laptops.findById({_id:id});
   const laptop = await Laptops.updateOne({_id:id},{assignedTo, remark, accessories});
   const result = await History.updateOne({_id:id},{$push:{assignHistory:getLaptopUser.assignedTo}});
-  res.status(200).json({ message: "data updated successfully" , data : result });
+  res.status(200).json({ message: "data updated successfully" , data : laptop });
 });
 
 router.delete("/delete/:id", async (req, res) => {
