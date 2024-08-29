8c62e7dd2f3b6585cc2bcc0077969a096b90a1cf
vish-codes <anand.panorama@gmail.com>
2024-08-29 16:25:20 +0530

    tst 1

--- db/index.js
+++ db/index.js
@@ -1,15 +1,16 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
-  .then(() => console.log("db connected successfully"));
+  .then(() => console.log('db connected successfully'));
 
 const adminSchema = new mongoose.Schema({
-  email: {  type: String,
+  email: {
+    type: String,
     required: true,
     unique: true,
     trim: true,
@@ -18,8 +19,7 @@ const adminSchema = new mongoose.Schema({
     maxLength: 30,
   },
   password: { type: String, required: true, minLength: 6 },
-})
-
+});
 
 /**
  * Account Schema
@@ -27,64 +27,66 @@ const adminSchema = new mongoose.Schema({
  * @requires mongoose
  */
 
-
 const laptopHistory = new mongoose.Schema({
   systemId: { type: String, required: true },
   laptopName: { type: String, required: true },
   empId: { type: String, required: true },
   assignedTo: { type: String, required: true },
-  fromDate:{type: String, required: true},
-  toDate:{type: String, required: true},
-  accessories : [
+  fromDate: { type: String, required: true },
+  toDate: { type: String, required: true },
+  accessories: [
     {
       type: String,
-      default: "None"
+      default: 'None',
     },
   ],
 });
 
-
-  /**
+/**
  * Account Schema
  * @module models/laptopSchema
  * @requires mongoose
  */
 
-
 const laptopSchema = new mongoose.Schema({
-  systemId:{ type: String, required: true},
+  systemId: { type: String, required: true },
   laptopName: {
     type: String,
     required: true,
     minLength: 3,
-  }, 
-  date:{type: String, required: true},
-  ownedBy: { type: String, required: true, default :"Company" },
-  ownerName: { type: String, default:"Panorama" },
-  accessories : [
+  },
+  laptopPass: { type: String, required: true, minLength: 1 },
+  date: { type: String, required: true },
+  ownedBy: { type: String, required: true, default: 'Company' },
+  ownerName: { type: String, default: 'Panorama' },
+  accessories: [
     {
       type: String,
-      default: "None"
+      default: 'None',
     },
   ],
-  assignedTo: { type: String, required: true, minLength: 2, trim: true, default: "N/A", required: true },
-  empId : { type: String, required: true, trim: true },
-  remark: { type: String, default: "None" },
-  history: [laptopHistory]
+  assignedTo: {
+    type: String,
+    required: true,
+    minLength: 2,
+    trim: true,
+    default: 'N/A',
+    required: true,
+  },
+  empId: { type: String, required: true, trim: true },
+  remark: { type: String, default: 'None' },
+  history: [laptopHistory],
 });
 
+laptopSchema.pre('save', function (next) {
+  if (this.ownerName.length === 0) this.ownerName = 'Panorama';
+  next();
+});
 
-
-laptopSchema.pre("save", function (next){
-  if(this.ownerName.length === 0) this.ownerName = "Panorama";
-  next()
-})
-
-const Admin = mongoose.model("Admin", adminSchema);
-const Laptops = mongoose.model("Laptops", laptopSchema);
+const Admin = mongoose.model('Admin', adminSchema);
+const Laptops = mongoose.model('Laptops', laptopSchema);
 // const History = mongoose.model("History", laptopHistory);
 
 export { Laptops, Admin };
-
 
 // history :- laptop history - username, userId, accessories, date
